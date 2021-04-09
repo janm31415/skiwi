130d1662250470b8cb353f0e2b5fe8a83f1693f8
Jan Maes <janm31415@gmail.com>
2021-04-09 22:47:10 +0200

    benchmarks

--- bench/benchall.scm
+++ bench/benchall.scm
@@ -5,19 +5,20 @@
 (import 'io)
 (import 'eval)
 
-
+(define new-benchmarks
+  '(conform))
 
 (define all-benchmarks
-   '(ack array1 boyer browse compiler cpstak ctak dderiv deriv 
+   '(ack array1 boyer browse cat compiler cpstak ctak dderiv deriv 
      destruc diviter dynamic earley fft fib fibc fibfp formattest 
      fpsum gcbench graphs lattice maze mazefun mbrot nboyer 
      nqueens ntakl paraffins parsing perm9 peval pnpoly primes 
      puzzle quicksort ray sboyer scheme simplex slatex string 
-     sum sumfp sumloop tail tak takl trav1 trav2 triangl)
+     sum sum1 sumfp sumloop tail tak takl trav1 trav2 triangl)
 )
 
 (define failing
-  '(matrix sum1 wc)
+  '(matrix wc)
 )
 
 (define main '())
@@ -35,5 +36,5 @@
   )
 )
 
-(for-each run-named-benchmark all-benchmarks)
+(for-each run-named-benchmark new-benchmarks)
 
