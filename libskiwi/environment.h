7a792fd85105eec6cc9e29998640310d8fe68d97
janm31415 <janm31415@gmail.com>
2021-04-12 10:33:51 +0200

    environment iterative

--- libskiwi/environment.h
+++ libskiwi/environment.h
@@ -19,11 +19,16 @@ class environment
     bool has(const std::string& name)
       {
       auto it = env.find(name);
-
       if (it != env.end())
         return true;
-      if (p_outer)
-        return p_outer->has(name);
+      auto p_outer_copy = p_outer;
+      while (p_outer_copy)
+        {
+        it = p_outer_copy->env.find(name);
+        if (it != p_outer_copy->env.end())
+          return true;
+        p_outer_copy = p_outer_copy->p_outer;
+        }
       return false;
       }
 
@@ -35,9 +40,16 @@ class environment
         e = it->second;
         return true;
         }
-      if (p_outer)
+      auto p_outer_copy = p_outer;
+      while (p_outer_copy)
         {
-        return p_outer->find(e, name);
+        it = p_outer_copy->env.find(name);
+        if (it != p_outer_copy->env.end())
+          {
+          e = it->second;
+          return true;
+          }
+        p_outer_copy = p_outer_copy->p_outer;
         }
       return false;
       }
@@ -51,9 +63,20 @@ class environment
         out = *it;
         return true;
         }
-      if (p_outer)
+      //if (p_outer)
+      //  {
+      //  return p_outer->find_if(out, p);
+      //  }
+      auto p_outer_copy = p_outer;
+      while (p_outer_copy)
         {
-        return p_outer->find_if(out, p);
+        it = std::find_if(p_outer_copy->env.begin(), p_outer_copy->env.end(), p);
+        if (it != p_outer_copy->env.end())
+          {
+          out = *it;
+          return true;
+          }
+        p_outer_copy = p_outer_copy->p_outer;
         }
       return false;
       }
@@ -66,8 +89,19 @@ class environment
         it->second = e;
         return true;
         }
-      if (p_outer)
-        return p_outer->replace(name, e);
+      auto p_outer_copy = p_outer;
+      while (p_outer_copy)
+        {
+        it = p_outer_copy->env.find(name);
+        if (it != p_outer_copy->env.end())
+          {
+          it->second = e;
+          return true;
+          }
+        p_outer_copy = p_outer_copy->p_outer;
+        }
+      //if (p_outer)
+      //  return p_outer->replace(name, e);
       return false;
       }
 
@@ -78,8 +112,18 @@ class environment
         {
         env.erase(it);
         }
-      if (p_outer)
-        p_outer->remove(name);
+      //if (p_outer)
+      //  p_outer->remove(name);
+      auto p_outer_copy = p_outer;
+      while (p_outer_copy)
+        {
+        it = p_outer_copy->env.find(name);
+        if (it != p_outer_copy->env.end())
+          {
+          p_outer_copy->env.erase(it);
+          }
+        p_outer_copy = p_outer_copy->p_outer;
+        }
       }
 
     void push(const std::string& name, TEntry e)
@@ -89,8 +133,19 @@ class environment
 
     void push_outer(const std::string& name, TEntry e)
       {
+      /*
       if (p_outer)
         p_outer->push_outer(name, e);
+      else
+        env[name] = e;
+      */
+      if (p_outer)
+        {
+        auto p_outer_copy = p_outer;
+        while (p_outer_copy->p_outer)
+          p_outer_copy = p_outer_copy->p_outer;
+        p_outer_copy->env[name] = e;
+        }
       else
         env[name] = e;
       }
@@ -107,6 +162,7 @@ class environment
 
     void rollup()
       {
+      /*
       if (p_outer)
         {
         p_outer->rollup();
@@ -115,6 +171,15 @@ class environment
         for (; it != it_end; ++it)
           env.insert(*it);
         p_outer.reset();
+        }
+      */
+      while (p_outer)
+        {
+        auto it = p_outer->begin();
+        auto it_end = p_outer->end();
+        for (; it != it_end; ++it)
+          env.insert(*it);
+        p_outer = p_outer->p_outer;
         }
       }
 
@@ -131,8 +196,14 @@ std::shared_ptr<environment<TEntry>> make_deep_copy(const std::shared_ptr<enviro
   if (!env.get())
     return env;
   std::shared_ptr<environment<TEntry>> out = std::make_shared<environment<TEntry>>(*env);
-  if (out->p_outer)
-    out->p_outer = make_deep_copy(out->p_outer);
+  //if (out->p_outer)
+  //  out->p_outer = make_deep_copy(out->p_outer);
+  auto* p_outer_copy = &(out->p_outer);
+  while (*p_outer_copy)
+    { // this loop is not tested anywhere yet
+    *p_outer_copy = std::make_shared<environment<TEntry>>(*p_outer_copy);
+    p_outer_copy = &((*p_outer_copy)->p_outer);
+    }
   return out;
   }
 
